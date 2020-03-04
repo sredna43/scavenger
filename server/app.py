5f8b0386649304050070e93169602fbe6c1ccda8
Anders Olson <sredna43@gmail.com>
2020-03-04 15:35:59 -0600

    runs

--- server/app.py
+++ server/app.py
@@ -1,8 +1,19 @@
-from flask import Flask, request
+# Import everything that we need from Flask
+from flask import Flask, request, session, flash
+# This may not be used, but it's for url parsing
 from markupsafe import escape
+# Store all of our data, potentially logins
+import sqlite3
+# Random vars
+import os
 
 app = Flask(__name__)
-app.secret_key = ""
+app.secret_key = os.urandom(16)
+
+@app.route('/')
+def home():
+    flash("Hello!")
+    return "If you're seeing this, you're in good shape"
 
 @app.route('/clue', methods=['POST'])
 def clue():
@@ -10,3 +21,4 @@ def clue():
         json_data = request.get_json()
         clue = json_data['clue']
         
+app.run(host='0.0.0.0', port='5000')
